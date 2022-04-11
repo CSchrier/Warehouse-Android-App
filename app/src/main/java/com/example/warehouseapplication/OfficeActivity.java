db78e8a1eb5ce55b85f1b74b3591e3d2e4953f00
bigwheel <dontemailme@me.com>
2022-04-11 19:35:02 -0400

    added search and edit activities for the office portion of app

--- app/src/main/java/com/example/warehouseapplication/OfficeActivity.java
+++ app/src/main/java/com/example/warehouseapplication/OfficeActivity.java
@@ -2,7 +2,9 @@ package com.example.warehouseapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class OfficeActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class OfficeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_office);
+    }
+
+    public void goToSearch(View view) {
+        Intent intent = new Intent(this, SearchActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToEditBay(View view) {
+        Intent intent = new Intent(this, EditActivity.class);
+        startActivity(intent);
     }
 }
