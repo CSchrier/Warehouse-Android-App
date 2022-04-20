1c3dd50a8d4fed5cafbc756fe9d33d694ab22524
Cameron <cameronschrier>
2022-04-20 17:22:32 -0400

    Updated InboundActivity

--- app/src/main/java/com/example/warehouseapplication/SearchActivity.java
+++ app/src/main/java/com/example/warehouseapplication/SearchActivity.java
@@ -3,12 +3,28 @@ package com.example.warehouseapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SearchActivity extends AppCompatActivity {
-
+    EditText jobText;
+    String text;
+    TextView output;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        jobText = (EditText) findViewById(R.id.jobSearch);
+        output = (TextView) findViewById(R.id.output);
+    }
+
+
+    public void search(View view) {
+        text = jobText.getText().toString();
+        //send some command to server with the job we want to know the location of
+
+
+        //take the response from the server and add it to our output
     }
 }
